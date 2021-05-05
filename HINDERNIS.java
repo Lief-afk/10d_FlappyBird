65f3612e15f067551f8fa82f270d36c84bdbdf5d
Arminlost <armin.miss@gmx.de>
2021-05-05 09:39:09 +0200

    Methoden für Boden müssen definiert werden. Nächste Woche: Versuch Boden erfolgreich zu erstellen.

---
+++ HINDERNIS.java
@@ -0,0 +1,26 @@
+
+/**
+ * Beschreiben Sie hier die Klasse HINDERNIS.
+ * 
+ * @author (Armin und Jaki) 
+ * @version (05.05.21)
+ */
+class HINDERNIS
+
+{
+    RECHTECK boden;
+    int x;
+    int y;
+
+
+    HINDERNIS()
+    {
+        boden = new RECHTECK();
+        boden.GroesseSetzen(10,5);
+        boden.FarbeSetzen("gruen");?
+            
+
+
+    }
+
+}
