bd471e3e2b8d3a9d7136d5e315d03a4de9dee2c4
Lief-afk <lgm.neo10@gmail.com>
2021-05-18 22:22:25 +0200

    BlueJ Projekt "shapes" abgeändert und für unsere Bedürfnisse angepasst Klasse Ellipse basiert auf Klasse Kreis

---
+++ Circle.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+/**
+ * A circle that can be manipulated and that draws itself on a canvas.
+ * 
+ * @author  Michael Kolling and David J. Barnes
+ * @version 1.0  (15 July 2000)
+ */
+
+public class Circle
+{
+    private int d;
+    private int xPos;
+    private int yPos;
+    private String color;
+    private boolean isVisible;
+    
+    /**
+     * Create a new circle at default position with default color.
+     */
+    public Circle(int d, int xPos, int yPos, String color)
+    {
+        this.d = d;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.color = color;
+    }
+    
+    /**
+     * Make this circle visible. If it was already visible, do nothing.
+     */
+    public void makeVisible()
+    {
+        isVisible = true;
+        draw();
+    }
+
+    /**
+     * Move the circle vertically by 'distance' pixels.
+     */
+    public void moveVertical(int distance)
+    {
+        erase();
+        yPos += distance;
+        draw();
+    }
+
+    /**
+     * Change the size to the new size (in pixels). Size must be >= 0.
+     */
+    public void changeSize(int newDiameter)
+    {
+        erase();
+        d = newDiameter;
+        draw();
+    }
+
+    /**
+     * Change the color. Valid colors are "red", "yellow", "blue", "green",
+     * "magenta" and "black".
+     */
+    public void changeColor(String newColor)
+    {
+        color = newColor;
+        draw();
+    }
+
+    /**
+     * Draw the circle with current specifications on screen.
+     */
+    private void draw()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.draw(this, color, new Ellipse2D.Double(xPos, yPos, 
+                                                             d, d));
+            canvas.wait(10);
+        }
+    }
+    
+    private void erase()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.erase(this);
+        }
+    }
+}
+
---
+++ Triangle.java
@@ -0,0 +1,85 @@
+
+import java.awt.*;
+
+/**
+ * A triangle that can be manipulated and that draws itself on a canvas.
+ * 
+ * @author    Michael Kolling and David J. Barnes
+ * @version 1.0  (15 July 2000)
+ */
+
+public class Triangle
+{
+    private int height;
+    private int width;
+    private int xPos;
+    private int yPos;
+    private String color;
+    private boolean isVisible;
+
+    /**
+     * Create a new triangle at default position with default color.
+     */
+    public Triangle(int width, int height, int xPos, int yPos, String color)
+    {
+        this.width = width;
+        this.height = height;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.color = color;
+    }
+
+    /**
+     * Make this triangle visible. If it was already visible, do nothing.
+     */
+    public void makeVisible()
+    {
+        isVisible = true;
+        draw();
+    }
+
+    /**
+     * Move the triangle vertically by 'distance' pixels.
+     */
+    public void moveVertical(int distance)
+    {
+        erase();
+        yPos += distance;
+        draw();
+    }
+
+    /**
+     * Change the color. Valid colors are "red", "yellow", "blue", "green",
+     * "magenta" and "black".
+     */
+    public void changeColor(String newColor)
+    {
+        color = newColor;
+        draw();
+    }
+
+    /*
+     * Draw the triangle with current specifications on screen.
+     */
+        private void draw()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            int[] xpoints = { xPos, xPos + (width/2), xPos - (width/2) };
+            int[] ypoints = { yPos, yPos + height, yPos + height };
+            canvas.draw(this, color, new Polygon(xpoints, ypoints, 3));
+            canvas.wait(10);
+        }
+    }
+
+    /*
+     * Erase the triangle on screen.
+     */
+    private void erase()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.erase(this);
+        }
+    }
+}
