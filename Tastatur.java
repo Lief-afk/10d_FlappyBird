785624297767c316dc73c403c57c3bf37a087234
Lief-afk <lgm.neo10@gmail.com>
2021-05-19 09:57:43 +0200

    ein erster Versuch an der Klasse Tastatur

---
+++ Tastatur.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ * Erstellen einer Tastatur zur wiedergabe der gedrückten Taste
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Tastatur
+{
+
+    /**
+     * Konstruktor für Objekte der Klasse Tastatur
+     */
+    public Tastatur()
+    {
+        
+    }
+    
+    void Taste(char  welche)
+    {
+        System. out. println ("Zeichen. " + welche);
+    }
+    
+    }
+
