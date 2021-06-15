bc3a7991a581d40b9e04f692cd22cba06db062fe
Arminlost <armin.miss@gmx.de>
2021-06-15 19:59:56 +0200

    Vogel geschrieben mit allen Formen. Noch nicht erstellt in Canvas.

--- Bird.java
+++ Bird.java
@@ -6,9 +6,19 @@
  */
 public class Bird
 {
-    
+    Circle auge;
+    Triangle schnabel;
+    Ellipse flügel;
+    Ellipse rumpf;
     public Bird()
     {
-
+        auge = new Circle(10,50,50,"black");
+        rumpf = new Ellipse(40,35,60,60,"red");
+        schnabel = new Triangle(12,12,70,70,"yellow");
+        flügel = new Ellipse(13,18,80,80,"white");
+        
+        
+        
+       
     }
 }
