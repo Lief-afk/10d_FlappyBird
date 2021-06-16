176b5ad1973c2d31c80268c03df514e4264876a3
ArminLost <armin.miss@gmx.de>
2021-06-16 09:17:37 +0200

    Röhren erstellt noch nicht auf Canvas erschienen.

---
+++ Tube.java
@@ -0,0 +1,29 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Tube.
+ * 
+ * @author (Jakob, Armin) 
+ * @version (1. Version, 16.06.21)
+ */
+public class Tube
+{
+   Square rechteck1;
+   Square rechteck2;
+   int xPosition;
+   int yPosition;
+   
+   Tube()
+   {
+       rechteck1 = new Square(50,450,300,0,"green");
+       rechteck2 = new Square(50,450,50,0,"green");
+       xPosition = 0;
+       yPosition = 0;
+       
+       
+    
+   }
+   public void draw()
+   {
+    
+    }
+}
