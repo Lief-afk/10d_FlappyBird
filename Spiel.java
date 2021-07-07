13ba675768aeae87ae5d1db7b7fbf0eee5c8fbc4
Lief-afk <lgm.neo10@gmail.com>
2021-07-07 09:38:30 +0200

    tastatur

--- Spiel.java
+++ Spiel.java
@@ -10,14 +10,13 @@ import javax.swing.JTextField;
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class Spiel
+public class Spiel extends JFrame implements KeyListener 
 {
     Bird bird;
     Tube tube1;
     Tube tube2;
     Tube tube3;
     Boolean gameover;
-    KeyEventClass tastatur;
     int beschleunigung;
     /**
      * Konstruktor für Objekte der Klasse Spiel
@@ -32,25 +31,48 @@ public class Spiel
         tube2.draw();
         tube3 = new Tube();
         tube3.draw();
-        tastatur = new KeyEventClass();
         bird.move(50); 
         beschleunigung = 1;
+        this.setLayout(new BorderLayout());
+        JTextField field = new JTextField();
+        field.addKeyListener(this);
+        this.add(field, BorderLayout.CENTER);
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }    
+    
+    public void keyTyped(KeyEvent e) 
+    {
+    }
+    
+    public void keyPressed(KeyEvent e) 
+    {
+        switch (e.getKeyChar())
+        {
+            case KeyEvent.VK_SPACE:
+            bird.move(-20);
+            break;
+            
+        }
+    }
+
+    public void keyReleased(KeyEvent e) 
+    {
+    }
     
     public void starteSpiel()
     {
         while (gameover == false)
         {
-            if (tastatur.e.getKeyEvent == Zeichen.VK_SPACE);
-            {
-                bird.move(-20);
-                tube1.bewegeNachLinks();
-                tube2.bewegeNachLinks();
-                tube3.bewegeNachLinks();
+            tube1.bewegeNachLinks();
+            tube2.bewegeNachLinks();
+            tube3.bewegeNachLinks();
+            bird.move(20);
             }
             }
-            }
-            }
+   }
          
 
 
